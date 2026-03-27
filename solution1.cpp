20d547d069ca3d1c475155fcc415c940d965b3f2
Vennela Varshini Anasoori <vennelavarshini07@gmail.com>
2026-03-27 21:15:06 +0530

    this might be the correct commit

--- solution1.cpp
+++ solution1.cpp
@@ -6,7 +6,7 @@ int main() {
     while(t--){
         long long n, k;
         cin >> n >> k;
-        cout << k + (k - 1) / (n - 1) << endl;
+        cout << k + (k / n) << endl; 
     }
     return 0;
 }
